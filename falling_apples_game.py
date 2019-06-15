9f3d2265ccbcc415473a2d19cdbad81028a4e4b4
maryyw <30440359+maryyw@users.noreply.github.com>
2019-06-15 12:23:09 -0400

    Update falling_apples_game.py

--- falling_apples_game.py
+++ falling_apples_game.py
@@ -1,134 +1,136 @@
 import random
 import arcade
+import os
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+
+timer = 0.0
 
 WIDTH = 1000
 HEIGHT = 650
 
-player_x = WIDTH/2
-player_y = HEIGHT/2
+apple_scale = 0.02
+basket_scale = 0.2
 
-apple_sprite = arcade.Sprite('images/apple.png', center_x=WIDTH/2, center_y=HEIGHT/2, scale=0.2)
+basket_sprite = arcade.Sprite('images/basket.png', basket_scale, center_x=WIDTH/2, center_y=HEIGHT/2)
 
-timer = 0.0
+BASKET_MOVEMENT_SPEED = 10
+APPLE_MOVEMENT_SPEED = 2
 
-apples_in_trees_list = []
-fallen_apples_list = []
+apples_in_trees_list = arcade.SpriteList()
+fallen_apples_list = arcade.SpriteList()
 
 fallen_apples_counter = 0
 apple_caught_counter = 0
 
 i = 0
 
-# controls
-up_is_being_pressed = False
-down_is_being_pressed = False
-left_pressed = False
-right_pressed = False
+apple_spawn_counter = 0
 
 
 def stopwatch():
     minutes = int(timer) // 60
     seconds = int(timer) % 60
     output = f"Time: {minutes:02d}:{seconds:02d}"
-    arcade.draw_text(output, 300, 300, arcade.color.BLACK, 30)
+    arcade.draw_text(output, 50, 20, arcade.color.BLACK, 30)
 
 
 def create_apples():
     for _ in range(6):
-        x = random.randint(5, 998)
-        y = random.randint(400, 640)
-        apples_in_trees_list.append([x, y])
-    #print(apples_in_trees_list)
+        apple_sprite = arcade.Sprite('images/apple1.png', apple_scale)
+        apple_sprite.center_x = random.randint(5, 998)
+        apple_sprite.center_y = random.randint(400, 640)
+        apples_in_trees_list.append(apple_sprite)
 
 
 def apple_counter():
     global fallen_apples_counter, i
     try:
-        if apples_in_trees_list[i][1] < 0:
+        #apple_falling()
+        falling_apple = apples_in_trees_list[i]
+        if falling_apple.center_y < 0:
             i += 1
             fallen_apples_counter += 1
-        elif apples_in_trees_list[i][1] == player_y and apples_in_trees_list[i][0] == player_x:
-            i += 1
-            fallen_apples_counter += 1
+            falling_apple.kill()
+        # elif falling_apple.center_y == basket_sprite.center_y and falling_apple.center_x == basket_sprite.center_x:
+        #     i += 1
+        #     fallen_apples_counter += 1
     except IndexError:
         pass
 
 
 def apple_falling():
-    global fallen_apples_counter
-    if fallen_apples_counter == 0:
-        apples_in_trees_list[0][1] -= 10
-    elif fallen_apples_counter == 1:
-        apples_in_trees_list[1][1] -= 10
-    elif fallen_apples_counter == 2:
-        apples_in_trees_list[2][1] -= 10
-    elif fallen_apples_counter == 3:
-        apples_in_trees_list[3][1] -= 10
-    elif fallen_apples_counter == 4:
-        apples_in_trees_list[4][1] -= 10
-    elif fallen_apples_counter == 5:
-        apples_in_trees_list[5][1] -= 10
+    try:
+        falling_apple = apples_in_trees_list[i]
+        falling_apple.center_y -= APPLE_MOVEMENT_SPEED
+    except IndexError:
+        pass
 
 
 def catch_apple():
-    global apple_caught_counter, i
-    if apples_in_trees_list[i][1] == player_y and apples_in_trees_list[i][0] == player_x:
-        apple_caught_counter += 1
-        apples_in_trees_list[i][0] = player_x
-        apples_in_trees_list[i][1] = player_y
-
-    print(apple_caught_counter)
-
-
-def add_to_fallen_apples_list():
-    global fallen_apples_counter, i
+    global apple_caught_counter
     try:
-        if apples_in_trees_list[i][1] < 0:
-            apples_in_trees_list.pop(i)
-            fallen_apples_list.append(i)
-        elif apples_in_trees_list[i][1] == player_y and apples_in_trees_list[i][0] == player_x:
-            apples_in_trees_list.pop(i)
-            fallen_apples_list.append(i)
+        falling_apple = apples_in_trees_list[i]
+        apples_list = arcade.check_for_collision(basket_sprite, falling_apple)
+        if apples_list:
+            falling_apple.kill()
+            #fallen_apples_list.append(falling_apple)
+            apple_caught_counter += 1
     except IndexError:
         pass
-    print(fallen_apples_list)
+
+
+def apple_spawn():
+    global apple_spawn_counter, i
+    i = 0
+    create_apples()
+    apple_spawn_counter += 1
+
+
+def apple_falling_speed_increase():
+    global APPLE_MOVEMENT_SPEED
+    if apple_spawn_counter:
+        APPLE_MOVEMENT_SPEED += 5
 
 
 def on_draw():
-    global apple
     arcade.start_render()
+    # Timer on screen
     stopwatch()
+    #apple_counter()
 
     # 6 Apples
     for apple in apples_in_trees_list:
-        arcade.draw_circle_filled(apple[0], apple[1], 20, arcade.color.RED)
+        apple.draw()
 
     # Basket
-    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.BLUE)
+    basket_sprite.draw()
+
+    # Apple Caught Counter
+    arcade.draw_text(f"Apple Caught: {int(apple_caught_counter)}", 50, 80, arcade.color.BLACK, 30)
 
 
 def on_key_press(key, modifiers):
-    global player_y, player_x, up_is_being_pressed, down_is_being_pressed, left_pressed, right_pressed
     if key == arcade.key.UP:
-        up_is_being_pressed = True
+        basket_sprite.change_y = BASKET_MOVEMENT_SPEED
     elif key == arcade.key.DOWN:
-        down_is_being_pressed = True
-    if key == arcade.key.LEFT:
-        left_pressed = True
+        basket_sprite.change_y = -BASKET_MOVEMENT_SPEED
+    elif key == arcade.key.LEFT:
+        basket_sprite.change_x = -BASKET_MOVEMENT_SPEED
     elif key == arcade.key.RIGHT:
-        right_pressed = True
+        basket_sprite.change_x = BASKET_MOVEMENT_SPEED
 
 
 def on_key_release(key, modifiers):
-    global player_y, player_x, up_is_being_pressed, down_is_being_pressed, left_pressed, right_pressed
     if key == arcade.key.UP:
-        up_is_being_pressed = False
+        basket_sprite.change_y = 0
     elif key == arcade.key.DOWN:
-        down_is_being_pressed = False
-    if key == arcade.key.LEFT:
-        left_pressed = False
+        basket_sprite.change_y = 0
+    elif key == arcade.key.LEFT:
+        basket_sprite.change_x = 0
     elif key == arcade.key.RIGHT:
-        right_pressed = False
+        basket_sprite.change_x = 0
 
 
 def on_mouse_press(x, y, button, modifiers):
@@ -136,19 +138,8 @@ def on_mouse_press(x, y, button, modifiers):
 
 
 def update(delta_time):
-    global timer, player_y, player_x, up_is_being_pressed, down_is_being_pressed
-    if up_is_being_pressed:
-        player_y += 10
-    elif down_is_being_pressed:
-        player_y -= 10
-
-    if left_pressed:
-        player_x -= 10
-    elif right_pressed:
-        player_x += 10
-
-    #print(player_x)
-    #print(player_y)
+    global timer
+    basket_sprite.update()
 
     stopwatch()
     timer += delta_time
@@ -156,12 +147,14 @@ def update(delta_time):
     apple_falling()
     catch_apple()
     apple_counter()
-    add_to_fallen_apples_list()
+    if not apples_in_trees_list:
+        apple_spawn()
+    #apple_falling_speed_increase()
 
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
-    arcade.set_background_color(arcade.color.WHITE)
+    arcade.set_background_color(arcade.color.SKY_BLUE)
     arcade.schedule(update, 1/60)
 
     # Override arcade window methods
